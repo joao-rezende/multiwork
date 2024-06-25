22d57ebb727cea1970a0b46d3f3db206b349d300
João Vitor S. Rezende <jvsrpassos@gmail.com>
2024-06-25 20:50:13 -0300

    Remove debugs

--- controllers/Orcamento.php
+++ controllers/Orcamento.php
@@ -12,13 +12,9 @@ class Orcamento
     public function imprimir()
     {
         if (empty($_POST)) return header('Location: ' . SITE_URL . "/orcamento");
-        // var_dump($_POST);
-        // exit();
+
         $css = file_get_contents(DIR_RAIZ . "/assets/css/pdfs/orcamento.css");
         $html = view("pdfs/orcamento", $_POST, TRUE);
-
-        // echo $html;
-        // die();
 
         $mpdf = new \Mpdf\Mpdf([
             "margin_top" => 10,

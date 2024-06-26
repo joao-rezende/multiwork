50efb3de48af2361b975b1f6842f0993cf13db71
João Vitor S. Rezende <jvsrpassos@gmail.com>
2024-06-25 22:10:24 -0300

    Fixed configs

--- config.php
+++ config.php
@@ -1,4 +1,3 @@
 <?php
-// const BASE_URL = "https://multiwork-0350785a3808.herokuapp.com";
-const BASE_URL = "http://localhost:8888/multiwork";
+const BASE_URL = "https://multiwork-0350785a3808.herokuapp.com";
 const DEBUG = FALSE;
